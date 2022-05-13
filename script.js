46b32a80724f3f490e2cbd7a97ab329303164601
Oppong200 <oppongnathaniel14@gmail.com>
2022-05-13 18:23:56 +0000

    Addition of FontAwesome, dropdown-menu and onscroll for the navbar

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll=function(){scrollfunction()}
+
+function scrollfunction(){
+    if(document.body.scrollTop>50 || document.documentElement.scrollTop>50){
+        document.getElementById('navbar').style.backgroundColor="#fff";
+    }
+    else{
+        document.getElementById('navbar').style.backgroundColor="none";
+    }
+}
